ac9b7d6e962742ec8638e871726e345ad9109c3f
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-12 21:47:39 -0700

    Sending a Name

--- client.js
+++ client.js
@@ -8,10 +8,16 @@ const connect = function() {
   
   conn.on("connect", () => {
     console.log("Connection established");
+    conn.write("Name: MI");
   });
 
   conn.on("data", (data) => {
     console.log("Server says: ", data);
+  });
+
+  conn.on("connect", (connect) => {
+    console.log("Successfully connected to game server");
+    
   });
 
   conn.setEncoding("utf8");
