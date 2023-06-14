b1f1cadf926ec2b3635dbe236e9904a48df46ffd
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-13 22:10:09 -0700

    WASD Movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,11 +11,22 @@ const setupInput = function() {
     if (key === '\u0003') {
       process.exit();
     }
+    if (key === "w") {
+      connection.write("Move: up");
+    } else if (key === "a") {
+      connection.write("Move: left");
+    } else if (key === "s") {
+      connection.write("Move: down");
+    } else if (key === "d") {
+      connection.write("Move: right");
+    }
   };
 
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => handleUserInput(key));
   return stdin;
 };
+
+
 
 module.exports = {
   setupInput
